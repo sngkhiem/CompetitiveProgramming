9c25ae5ff9c01941408988a80524ba771a5c6b98
Khiem Tran <sngkhang146@gmail.com>
2020-12-25 22:18:00 +0700

    Add solution for problem `A - Design Tutorial: Learn from Math` Link: https://codeforces.com/contest/472/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/472/submission/102330284

--- codeforces/472/A.cpp
+++ codeforces/472/A.cpp
@@ -1,11 +1,55 @@
-#include <bits/stdc++.h>
+/*
+	Author: sngkhiem
+*/
+#define _CRT_SECURE_NO_WARNINGS
 
-using namespace std; 
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
 
-signed main() {
-	int n;
+using namespace std;\
+
+/*fast io*/
+#define fastIO ios::sync_with_stdio(false); cin.tie(0);
+
+/*push-pos define*/
+#define pb push_back
+#define pob pop_back
+#define fi first
+#define se second
+
+/*for define*/
+#define for(i,a,b) for(int i=a; i<=b; i++)
+#define bor(i,a,b) for(int i=a; i>=n; i--)
+
+/*typedef*/
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long l;
+typedef unsigned long ul;
+typedef long double ld;
+typedef int i;
+typedef unsigned int ui;
+typedef string s;
+typedef char c;
+typedef bool b;
+typedef float f;
+typedef unsigned short us;
+
+int main() {
+	fastIO;
+	#ifndef ONLINE_JUDGE
+		freopen("inp.txt", "r", stdin);
+		freopen("out.txt", "w", stdout);
+	#endif
+	i n;
 	cin >> n;
-	if (n % 2 == 0) cout << 4 << " " << n - 4;
-	else cout << 9 << " " << n - 9;
+	if (n % 2 == 0) cout << n - 4 << " " << 4;
+	else cout << n - 9 << " " << 9;
 	return 0;
 }
