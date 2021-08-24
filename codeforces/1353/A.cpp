890ff347609e6d3b822a2cc0bdbc6db2183aeb78
Khiem Tran <sngkhang146@gmail.com>
2021-08-24 21:29:00 +0700

    Add solution for problem `A - Most Unstable Array` Link: https://codeforces.com/contest/1353/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1353/submission/126846785

---
+++ codeforces/1353/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main(){
+    int t; cin >> t;
+    while (t--) {
+        int n,m;
+        cin >> n >> m;
+        if (n == 1) cout << 0 << endl;
+        else if (n <= 2) cout << m << endl;
+        else cout << m*2 << endl;
+    }
+}
