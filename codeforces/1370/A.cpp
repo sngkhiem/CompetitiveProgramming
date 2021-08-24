3598ddee3bfbdc74527dc9f0b6970ab049a19fd8
Khiem Tran <sngkhang146@gmail.com>
2021-08-24 20:28:00 +0700

    Add solution for problem `A - Maximum GCD` Link: https://codeforces.com/contest/1370/problem/A Tags: greedy, implementation, math, number theory, *800 Ref: https://codeforces.com/contest/1370/submission/126842289

---
+++ codeforces/1370/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    int t; cin >> t;
+    while (t--) {
+        int cur;
+        cin >> cur;
+        cout << cur/2 << endl;
+    }
+}
