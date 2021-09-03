4cc7271e1cd26f667bd20690cda4c8d33e2dec84
Khiem Tran <sngkhang146@gmail.com>
2021-09-03 16:43:00 +0700

    Add solution for problem `A - Juggling Letters` Link: https://codeforces.com/contest/1397/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1397/submission/127755597

--- codeforces/1397/A.cpp
+++ codeforces/1397/A.cpp
@@ -1,30 +1,28 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-int t,n;
- 
-int main()
-{
-    cin >> t;
-    while (t--)
-    {
-        cin >> n;
-        int a[151],c=0;
-        fill_n(a,150,0);
-        for (int i=1;i<=n;i++)
-        {
-            string s;
-            cin >> s;
-            for (int j=0;j<s.size();j++) a[s[j]]++;
+
+//#define int long long
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+//const int INF = 1e18;
+
+signed main() {
+    fastIO;
+    int t; cin >> t;
+    while (t--) {
+        int p[1005],flag=0;
+        fill(p,p+1004,0);
+        int n; cin >> n;
+        for (int i = 0; i < n; i++) {
+            string s; cin >> s;
+            for (int j = 0; j < s.size(); j++) p[int(s[j])]++;
         }
-        for (int i=97;i<=142;i++)
-            if (a[i]!=0 && a[i]%n!=0)
-            {
-                cout << "NO\n";
-                c=-1;
-                break;
-            }
-        if (c==0) cout << "YES\n";
+        for (int i = 0; i < 1004; i++)
+            if (p[i] % n != 0) {
+                flag = 1; cout << "NO\n"; break;
+            } 
+        if (flag == 0) cout << "YES\n";
     }
+    return 0;
 }
