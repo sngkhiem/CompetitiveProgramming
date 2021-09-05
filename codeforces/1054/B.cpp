ebfa3fa2d3524a27bfb5f5edec8b47667e9a175e
Khiem Tran <sngkhang146@gmail.com>
2021-09-05 19:39:00 +0700

    Add solution for problem `B - Appending Mex` Link: https://codeforces.com/contest/1054/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/1054/submission/127914540

--- codeforces/1054/B.cpp
+++ codeforces/1054/B.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-int n,k,t,tmp;
- 
-int main()
-{
-    cin >> n;
-    cin >> tmp;
-    if (tmp!=0) { cout << 1; return 0;}
-    else
-    {
-        t=0;
-        for (int i=2;i<=n;i++)
-        {
-            cin >> tmp;
-            if (tmp-t>1) 
-            {
-                cout << i;
-               	return 0;
+
+signed main() {
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    if (a[0] != 0) {cout << 1; return 0;}
+    else {
+        int ma = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] > ma + 1) {
+                cout << i+1; return 0;
             }
-            t=max(t,tmp);
+            ma = max(ma,a[i]);
         }
-        cout << -1;
     }
+    cout << -1; return 0;
+    return 0;
 }
