0416e57f4dbdaeb0b0f9f8df9da127a98874f59e
Trần Hoàng Khiêm <sngkhang146@gmail.com>
2021-09-11 23:32:46 +0700

    Create twostones.cpp

---
+++ Kattis/twostones.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+    int n; cin >> n;
+    if (n % 2 == 0) cout << "Bob";
+    else cout << "Alice";
+    return 0;
+}
