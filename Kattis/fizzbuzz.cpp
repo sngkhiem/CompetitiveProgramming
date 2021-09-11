63b482fca72588e9f8d0995d2c7b1fe175a97cdc
Trần Hoàng Khiêm <sngkhang146@gmail.com>
2021-09-11 23:33:23 +0700

    Create fizzbuzz.cpp

---
+++ Kattis/fizzbuzz.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    int x,y,n; cin >> x >> y >> n;
+    for (int i = 1; i <= n; i++) {
+        if (i % x == 0 && i % y == 0) cout << "FizzBuzz\n";
+        else if (i % x == 0) cout << "Fizz\n";
+        else if (i % y == 0) cout << "Buzz\n";
+        else cout << i << "\n";
+    }
+    return 0;
+}
