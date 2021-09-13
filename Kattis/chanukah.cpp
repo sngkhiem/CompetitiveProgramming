22489c3b6e921f41792288800a286c3a25309549
Trần Hoàng Khiêm <sngkhang146@gmail.com>
2021-09-13 20:46:37 +0700

    Create chanukah.cpp

---
+++ Kattis/chanukah.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    int t; cin >> t;
+    while (t--) {
+        int k,n; cin >> k >> n;
+        cout << k << " " << n*(n+1)/2+n << "\n";
+    }
+    return 0;
+}
