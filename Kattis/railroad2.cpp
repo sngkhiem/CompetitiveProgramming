74d3a0c269c1ca55b084451e9a2cd8eeb7c04b61
Trần Hoàng Khiêm <sngkhang146@gmail.com>
2021-09-23 21:17:45 +0700

    Create railroad2.cpp

---
+++ Kattis/railroad2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    int x,y; cin >> x >> y;
+    if (y % 2 == 1) cout << "impossible";
+    else cout << "possible";
+    return 0;
+}
