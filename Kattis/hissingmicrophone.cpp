b47140fa9ce097cf0f4396cff45abd93dcc5dff5
Trần Hoàng Khiêm <sngkhang146@gmail.com>
2021-09-24 09:17:25 +0700

    Create hissingmicrophone.cpp

---
+++ Kattis/hissingmicrophone.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    string s; cin >> s;
+    for (int i = 0; i < s.size()-1; i++)
+        if (s[i] == 's' && s[i] == s[i+1]) {
+            cout << "hiss"; return 0;
+        }
+    cout << "no hiss";
+    return 0;
+}
