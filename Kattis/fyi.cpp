caa142c2c61139bdb5a9f0aba8abfc78433fabab
Trần Hoàng Khiêm <sngkhang146@gmail.com>
2021-09-25 12:56:46 +0700

    Create fyi.cpp

---
+++ Kattis/fyi.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    string s; cin >> s;
+    if (s[0] == '5' && s[1] == '5' && s[2] == '5') cout << 1;
+    else cout << 0;
+    return 0;
+}
