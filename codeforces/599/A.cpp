e1b82c975ceffd858c74caf274f72c1ca8150e08
Trần Hoàng Khiêm <58633954+sngkhiem@users.noreply.github.com>
2022-04-27 20:31:09 +0700

    Delete codeforces directory

--- codeforces/599/A.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-signed main() {
-    int d1,d2,d3; cin >> d1 >> d2 >> d3;
-    cout << min(d1+d2+d3,min(d1*2+d2*2,min(d1*2+d3*2,d2*2+d3*2)));
-    return 0;
-}
--- codeforces/723/A.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-signed main() {
-    int a[4]; cin >> a[1] >> a[2] >> a[3];
-    sort(a+1,a+1+3);
-    cout <<( a[3] - a[2] ) + (a[2] - a[1]);
-}
